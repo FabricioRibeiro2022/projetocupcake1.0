464cfdccd84d27d4e34a1a4d24e9e7ce5a8888fc
FabricioRibeiro2022 <fabricio.romeu@gmail.com>
2022-10-29 22:57:47 -0300

    projeto cupcake1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BemVindoComponent } from './bem-vindo/bem-vindo.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { CardapioComponent } from './cardapio/cardapio.component';
+import { HomeComponent } from './home/home.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'bem-vindo',
+    component: BemVindoComponent
+  },
+
+  {
+    path: 'cadastro',
+    component: CadastroComponent
+  },
+
+  {
+    path: 'cardapio',
+    component: CardapioComponent
+  },
+
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+
+  {
+    path: 'sobre-nos',
+    component: SobreNosComponent
+  },
+
+  // Este abaixo irá redirecionar qualquer caminho não listado acima para o 'home'.
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BemVindoComponent } from './bem-vindo/bem-vindo.component';
+import { HomeComponent } from './home/home.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { CardapioComponent } from './cardapio/cardapio.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BemVindoComponent,
+    HomeComponent,
+    CadastroComponent,
+    CardapioComponent,
+    SobreNosComponent
   ],
   imports: [
     BrowserModule,
